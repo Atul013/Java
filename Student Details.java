4303f9fd7cf8d044f0101af0c71e06156d112d31
Atul Biju <141022002+Atul013@users.noreply.github.com>
2024-07-23 22:03:07 +0530

    Update and rename student.java to Student Details.java

--- Student Details.java
+++ Student Details.java
@@ -4,12 +4,16 @@ class student
 	public static void main(String args[])
 	{
 		Scanner s1 = new Scanner(System.in);
-		System.out.println("Enter your Roll Number : ");
-		int rollno = s1.nextInt();
+        String cls, name;
 		System.out.println("Enter your Name : ");
 		name = s1.nextLine();
 		System.out.println("Enter your Class : ");
-		class = s1.nextLine();
+		cls = s1.nextLine();
+        System.out.println("Enter your Roll Number : ");
+		int rollno = s1.nextInt();
 		System.out.println("Name : " +name);
-		System.out.println("Class : " +class);
+		System.out.println("Class : " +cls);
 		System.out.println("Roll Number : " +rollno);
+        s1.close();
+    }
+}
