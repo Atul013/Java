7030154aaf2776e8cba883cdc0f9bf917d2ccaa2
Atul Biju <141022002+Atul013@users.noreply.github.com>
2024-07-23 12:29:55 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class student
+{
+	public static void main(String args[])
+	{
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter your Roll Number : ");
+		int rollno = s1.nextInt();
+		System.out.println("Enter your Name : ");
+		name = s1.nextLine();
+		System.out.println("Enter your Class : ");
+		class = s1.nextLine();
+		System.out.println("Name : " +name);
+		System.out.println("Class : " +class);
+		System.out.println("Roll Number : " +rollno);
